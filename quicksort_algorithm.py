0ce0ae9eb760e1a008c4daea51fc77fb5acc8733
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-02-04 20:58:45 +0100

    footer

--- quicksort_algorithm.py
+++ quicksort_algorithm.py
@@ -8,7 +8,6 @@ def quicksort(arr):
         greater_than_pivot = [x for x in arr[1:] if x > pivot]
         return quicksort(less_than_pivot) + [pivot] + quicksort(greater_than_pivot)
 
-# User Input
 user_input = input("Enter a list of numbers separated by spaces: ")
 try:
     user_array = [int(x) for x in user_input.split()]
@@ -17,3 +16,4 @@ try:
     print(f"Sorted Array: {sorted_array}")
 except ValueError:
     print("Invalid input. Please enter a list of numbers.")
+# 2024 Peace Oloruntoba.
